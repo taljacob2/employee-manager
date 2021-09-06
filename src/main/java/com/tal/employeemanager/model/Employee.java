0670858673037b721bc308ee8d56bb52aa1b96c4
Tal Jacob <taljacob2@gmail.com>
2021-09-06 05:40:38 +0300

    init 2

---
+++ src/main/java/com/tal/employeemanager/model/Employee.java
@@ -0,0 +1,22 @@
+package com.tal.employeemanager.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data @Entity @Table public class Employee implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    @Id @GeneratedValue @Column(nullable = false, updatable = false)
+    @Setter(AccessLevel.NONE) private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageURL;
+    @Column(nullable = false, updatable = false) private String code;
+}
