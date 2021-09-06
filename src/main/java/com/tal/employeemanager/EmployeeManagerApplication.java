60737da07946a460e9a177a8fd54d1bf08233aeb
Tal Jacob <taljacob2@gmail.com>
2021-09-06 05:40:58 +0300

    init 2

--- src/main/java/com/tal/employeemanager/EmployeeManagerApplication.java
+++ src/main/java/com/tal/employeemanager/EmployeeManagerApplication.java
@@ -3,6 +3,9 @@ package com.tal.employeemanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Guide: https://www.youtube.com/watch?v=Gx4iBLKLVHk&t=757s
+ */
 @SpringBootApplication public class EmployeeManagerApplication {
 
     public static void main(String[] args) {
