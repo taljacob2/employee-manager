ab3fd39b72fb52bdef2042ab9d377921a5b4f3c5
Tal Jacob <taljacob2@gmail.com>
2021-09-06 05:14:48 +0300

    Initial commit

---
+++ src/main/java/com/tal/employeemanager/EmployeeManagerApplication.java
@@ -0,0 +1,12 @@
+package com.tal.employeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class EmployeeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagerApplication.class, args);
+    }
+
+}
