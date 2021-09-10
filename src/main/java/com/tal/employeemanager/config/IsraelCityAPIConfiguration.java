3a2e809e7470d120ba1db42cb4bf80305b1126d7
Tal Jacob <taljacob2@gmail.com>
2021-09-10 21:46:51 +0300

    after getAPI 2

--- src/main/java/com/tal/employeemanager/config/IsraelCityAPIConfiguration.java
+++ src/main/java/com/tal/employeemanager/config/IsraelCityAPIConfiguration.java
@@ -1,26 +1,36 @@
 package com.tal.employeemanager.config;
 
+import com.tal.employeemanager.entity.israelcityapi.Record;
 import com.tal.employeemanager.entity.israelcityapi.Root;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-@Slf4j @Controller("/") public class IsraelCityAPIConfiguration {
+import java.util.List;
+
+@Slf4j @Data @Configuration public class IsraelCityAPIConfiguration
+        implements CommandLineRunner {
 
     private final RestTemplate restTemplate = new RestTemplate();
     @Value("${israel.city.api.link}") String israelCityAPILink;
+    private List<Record> records = null;
 
-    @GetMapping @ResponseBody Root getAPI() {
-        Root root = null;
+    @Override public void run(String... args) throws Exception {
+        extractAPI();
+    }
+
+    private void extractAPI() {
         try {
-            root = restTemplate.getForObject(israelCityAPILink, Root.class);
+            Root root =
+                    restTemplate.getForObject(israelCityAPILink, Root.class);
+            records = root.getResult().getRecords();
         } catch (HttpClientErrorException e) {
             log.error(e.getResponseBodyAsString());
         }
-        return root;
     }
+
 }
