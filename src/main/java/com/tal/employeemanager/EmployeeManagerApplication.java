d1180b866551e67753cf4b8cb4e49117d5b4179d
Tal Jacob <taljacob2@gmail.com>
2021-09-22 11:04:23 +0300

    after adding record.properties

--- src/main/java/com/tal/employeemanager/EmployeeManagerApplication.java
+++ src/main/java/com/tal/employeemanager/EmployeeManagerApplication.java
@@ -2,10 +2,12 @@ package com.tal.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Guide: https://www.youtube.com/watch?v=Gx4iBLKLVHk&t=757s
  */
+@PropertySource(value = {"/application.properties", "/record.properties"})
 @SpringBootApplication public class EmployeeManagerApplication {
 
     public static void main(String[] args) {
